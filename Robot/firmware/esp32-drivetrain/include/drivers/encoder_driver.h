302e29e7c9565af8500e9aace69151254516f2e0
Jimmy Pan <jimmypan0410@gmail.com>
2026-07-13 15:27:32 -0700

    JP: small encoder fix

--- Robot/firmware/esp32-drivetrain/include/drivers/encoder_driver.h
+++ Robot/firmware/esp32-drivetrain/include/drivers/encoder_driver.h
@@ -42,7 +42,7 @@ typedef struct {
 } EncoderDriverConfig;
 
 typedef struct {
-    EncoderDriverConfig *config;
+    const EncoderDriverConfig *config;
 
     int32_t accumulated_count;
     int64_t last_timestamp_us;
